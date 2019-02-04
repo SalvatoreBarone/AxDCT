175a0d422e92c3df8a2e9e7bbe92b8af7e98e77c
Andrea Aletto <andrea.aletto8@gmail.com>
2019-02-04 21:58:24 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+
 int main(int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data. Path is: %s\n", argv[1]);
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
+    assert( argc == 2 && "usage: displayImg <Image_Path>\n");
+
+    Mat image = imread( argv[1], 1 );
+
+    assert( image.data && "No image data")
+
+    namedWindow("Original Image", WINDOW_AUTOSIZE );
+    imshow("Original Image", image);
+
+
+    //TODO: In image ho la matrice dei pixel dell'immagine
+    
+
     waitKey(0);
     return 0;
 }
