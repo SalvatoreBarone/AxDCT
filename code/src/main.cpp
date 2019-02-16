cee23e7bed1ff3812eadc2208d902c838afced35
Andrea Aletto <andrea.aletto8@gmail.com>
2019-02-16 11:44:24 +0100

    DCT-1D with matrix factorization

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -296,6 +296,8 @@ void AxDCT1D(const cv::Mat& input, cv::Mat& output){
     assert(( (input.rows == 8) && (input.cols==1) ) && "Column vector of size 8x1 is needed for 1D-DCT.");
     assert( (input.type() == CV_16S) && "Unable to compute AxDCT-1D: element of type CV_16S required.");
 
+    /***** DIRECT TRANSFORMATION X=T*x --- 24 adds *****/
+/*
     int16_t x0 = input.at<int16_t>(0,0);
     int16_t x1 = input.at<int16_t>(1,0);
     int16_t x2 = input.at<int16_t>(2,0);
@@ -313,6 +315,54 @@ void AxDCT1D(const cv::Mat& input, cv::Mat& output){
     output.at<int16_t>(5,0) = x6 + (-x1);
     output.at<int16_t>(6,0) = x2 + (-x1) + x5 + (-x6);
     output.at<int16_t>(7,0) = x4 + (-x3);
+*/
+
+
+    /****** Transformation using matrix factorization X = P*A3*A2*A1 * x --- 14 adds + 4 sign inversions ******/
+    int16_t x0a = input.at<int16_t>(0,0);
+    int16_t x1a = input.at<int16_t>(1,0);
+    int16_t x2a = input.at<int16_t>(2,0);
+    int16_t x3a = input.at<int16_t>(3,0);
+    int16_t x4a = input.at<int16_t>(4,0);
+    int16_t x5a = input.at<int16_t>(5,0);
+    int16_t x6a = input.at<int16_t>(6,0);
+    int16_t x7a = input.at<int16_t>(7,0);
+
+    int16_t x0b = x0a + x7a;
+    int16_t x1b = x1a + x6a;
+    int16_t x2b = x2a + x5a;
+    int16_t x3b = x3a + x4a;
+    int16_t x4b = x3a + (-x4a);
+    int16_t x5b = x2a + (-x5a);
+    int16_t x6b = x1a + (-x6a);
+    int16_t x7b = x0a + (-x7a);
+
+    int16_t x0c = x0b + x3b;
+    int16_t x1c = x1b + x2b;
+    int16_t x2c = x1b + (-x2b);
+    int16_t x3c = x0b + (-x3b);
+    int16_t x4c = -x4b;
+    int16_t x5c = -x5b;
+    int16_t x6c = -x6b;
+    int16_t x7c = x7b;
+
+    int16_t x0d = x0c + x1c;
+    int16_t x1d = x0c + (-x1c);
+    int16_t x2d = -x2c;
+    int16_t x3d = x3c;
+    int16_t x4d = x4c;
+    int16_t x5d = x5c;
+    int16_t x6d = x6c;
+    int16_t x7d = x7c;
+
+    output.at<int16_t>(0,0) = x0d;
+    output.at<int16_t>(1,0) = x7d;
+    output.at<int16_t>(2,0) = x3d;
+    output.at<int16_t>(3,0) = x5d;
+    output.at<int16_t>(4,0) = x1d;
+    output.at<int16_t>(5,0) = x6d;
+    output.at<int16_t>(6,0) = x2d;
+    output.at<int16_t>(7,0) = x4d;
     
 }
 
