66cc0e6260ff5376683ed85f4633779d19f6028f
Andrea Aletto <andrea.aletto8@gmail.com>
2019-02-18 13:52:17 +0100

    bugfix BAS08/09

---
+++ code/include/algorithms/BAS08.h
@@ -0,0 +1,56 @@
+//
+// AxDCT - A collection of inexact DCT Algorithms
+// Copyright (C) 2019 Andrea Aletto <andrea.aletto8@gmail.com>
+//
+// This file is part of AxDCT.
+//
+// AxDCT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AxDCT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with AxDCT.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/******************************************************************************
+ * @file   BAS08.h
+ * @author Andrea Aletto
+ * @date   11 feb 2019
+ * @brief  Declaration of BAS08 algorithm class
+ ******************************************************************************/
+
+#ifndef _BAS08_H
+#define _BAS08_H
+
+#include <opencv2/opencv.hpp>
+#include "AxDCT_algorithm.h"
+
+class BAS08 : public AxDCT_algorithm
+{
+
+    private:
+        void dct1d(const cv::Mat& input, cv::Mat& output);
+
+        
+        cv::Mat getD();
+        cv::Mat getQ();
+        cv::Mat getCQ();
+
+    public:
+        BAS08() : AxDCT_algorithm() {};
+        void y_quantizate(const cv::Mat&, cv::Mat&);
+        void cr_quantizate(const cv::Mat&, cv::Mat&);
+        void cb_quantizate(const cv::Mat&, cv::Mat&);
+
+        void y_dequantizate(const cv::Mat&, cv::Mat&);
+        void cr_dequantizate(const cv::Mat&, cv::Mat&);
+        void cb_dequantizate(const cv::Mat&, cv::Mat&);
+};
+
+#endif /* _BAS08_H */
---
+++ code/src/algorithms/BAS08.cpp
@@ -0,0 +1,300 @@
+//
+// AxDCT - A collection of inexact DCT Algorithms
+// Copyright (C) 2019 Andrea Aletto <andrea.aletto8@gmail.com>
+//
+// This file is part of AxDCT.
+//
+// AxDCT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// AxDCT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with AxDCT.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/******************************************************************************
+ * @file   BAS08.cpp
+ * @author Andrea Aletto
+ * @date   11 feb 2019
+ * @brief  Implementation of BAS08 algorithm class
+ ******************************************************************************/
+
+#include "BAS08.h"
+
+static void quantizate(const cv::Mat&, const cv::Mat&, const cv::Mat&, cv::Mat&);
+static void dequantizate(const cv::Mat&, const cv::Mat&, cv::Mat&);
+
+cv::Mat BAS08::getD(){
+
+    cv::Mat D = cv::Mat::zeros(8,8,CV_64FC1);
+
+    D.at<double>(0, 0) = 1/sqrt(8);
+    D.at<double>(1, 1) = 1/sqrt(2)      ;
+    D.at<double>(2, 2) = 1/sqrt(5);
+    D.at<double>(3, 3) = 1/sqrt(2);
+    D.at<double>(4, 4) = 1/sqrt(8);
+    D.at<double>(5, 5) = 0.5      ;
+    D.at<double>(6, 6) = 1/sqrt(5);
+    D.at<double>(7, 7) = 1/sqrt(2);   
+
+    return D;
+}
+
+cv::Mat BAS08::getQ(){
+
+    cv::Mat Q = cv::Mat::zeros(8,8,CV_64FC1);
+
+    Q.at<double>(0, 0) = 16;
+    Q.at<double>(1, 0) = 12;
+    Q.at<double>(2, 0) = 14;
+    Q.at<double>(3, 0) = 14;
+    Q.at<double>(4, 0) = 18;
+    Q.at<double>(5, 0) = 24;
+    Q.at<double>(6, 0) = 49;
+    Q.at<double>(7, 0) = 72;
+
+    Q.at<double>(0, 1) = 11;
+    Q.at<double>(1, 1) = 12;
+    Q.at<double>(2, 1) = 13;
+    Q.at<double>(3, 1) = 17;
+    Q.at<double>(4, 1) = 22;
+    Q.at<double>(5, 1) = 35;
+    Q.at<double>(6, 1) = 64;
+    Q.at<double>(7, 1) = 92;
+
+    Q.at<double>(0, 2) = 10;
+    Q.at<double>(1, 2) = 14;
+    Q.at<double>(2, 2) = 16;
+    Q.at<double>(3, 2) = 22;
+    Q.at<double>(4, 2) = 37;
+    Q.at<double>(5, 2) = 55;
+    Q.at<double>(6, 2) = 78;
+    Q.at<double>(7, 2) = 95;
+
+    Q.at<double>(0, 3) = 16;
+    Q.at<double>(1, 3) = 19;
+    Q.at<double>(2, 3) = 24;
+    Q.at<double>(3, 3) = 29;
+    Q.at<double>(4, 3) = 56;
+    Q.at<double>(5, 3) = 64;
+    Q.at<double>(6, 3) = 87;
+    Q.at<double>(7, 3) = 98;
+
+    Q.at<double>(0, 4) = 24;
+    Q.at<double>(1, 4) = 26;
+    Q.at<double>(2, 4) = 40;
+    Q.at<double>(3, 4) = 51;
+    Q.at<double>(4, 4) = 68;
+    Q.at<double>(5, 4) = 81;
+    Q.at<double>(6, 4) = 103;
+    Q.at<double>(7, 4) = 112;
+
+    Q.at<double>(0, 5) = 40;
+    Q.at<double>(1, 5) = 58;
+    Q.at<double>(2, 5) = 57;
+    Q.at<double>(3, 5) = 87;
+    Q.at<double>(4, 5) = 109;
+    Q.at<double>(5, 5) = 104;
+    Q.at<double>(6, 5) = 121;
+    Q.at<double>(7, 5) = 100;
+
+    Q.at<double>(0, 6) = 51;
+    Q.at<double>(1, 6) = 60;
+    Q.at<double>(2, 6) = 69;
+    Q.at<double>(3, 6) = 80;
+    Q.at<double>(4, 6) = 103;
+    Q.at<double>(5, 6) = 113;
+    Q.at<double>(6, 6) = 120;
+    Q.at<double>(7, 6) = 103;
+
+    Q.at<double>(0, 7) = 61;
+    Q.at<double>(1, 7) = 55;
+    Q.at<double>(2, 7) = 56;
+    Q.at<double>(3, 7) = 62;
+    Q.at<double>(4, 7) = 77;
+    Q.at<double>(5, 7) = 92;
+    Q.at<double>(6, 7) = 101;
+    Q.at<double>(7, 7) = 99;
+
+    return Q;
+}
+
+cv::Mat BAS08::getCQ(){
+
+    cv::Mat CQ = cv::Mat::zeros(8,8,CV_64FC1);
+
+    CQ.at<double>(0, 0) = 17;
+    CQ.at<double>(1, 0) = 18;
+    CQ.at<double>(2, 0) = 24;
+    CQ.at<double>(3, 0) = 47;
+    CQ.at<double>(4, 0) = 99;
+    CQ.at<double>(5, 0) = 99;
+    CQ.at<double>(6, 0) = 99;
+    CQ.at<double>(7, 0) = 99;
+
+    CQ.at<double>(0, 1) = 18;
+    CQ.at<double>(1, 1) = 21;
+    CQ.at<double>(2, 1) = 26;
+    CQ.at<double>(3, 1) = 66;
+    CQ.at<double>(4, 1) = 99;
+    CQ.at<double>(5, 1) = 99;
+    CQ.at<double>(6, 1) = 99;
+    CQ.at<double>(7, 1) = 99;
+
+    CQ.at<double>(0, 2) = 24;
+    CQ.at<double>(1, 2) = 26;
+    CQ.at<double>(2, 2) = 56;
+    CQ.at<double>(3, 2) = 99;
+    CQ.at<double>(4, 2) = 99;
+    CQ.at<double>(5, 2) = 99;
+    CQ.at<double>(6, 2) = 99;
+    CQ.at<double>(7, 2) = 99;
+
+    CQ.at<double>(0, 3) = 47;
+    CQ.at<double>(1, 3) = 66;
+    CQ.at<double>(2, 3) = 99;
+    CQ.at<double>(3, 3) = 99;
+    CQ.at<double>(4, 3) = 99;
+    CQ.at<double>(5, 3) = 99;
+    CQ.at<double>(6, 3) = 99;
+    CQ.at<double>(7, 3) = 99;
+
+    CQ.at<double>(0, 4) = 99;
+    CQ.at<double>(1, 4) = 99;
+    CQ.at<double>(2, 4) = 99;
+    CQ.at<double>(3, 4) = 99;
+    CQ.at<double>(4, 4) = 99;
+    CQ.at<double>(5, 4) = 99;
+    CQ.at<double>(6, 4) = 99;
+    CQ.at<double>(7, 4) = 99;
+
+    CQ.at<double>(0, 5) = 99;
+    CQ.at<double>(1, 5) = 99;
+    CQ.at<double>(2, 5) = 99;
+    CQ.at<double>(3, 5) = 99;
+    CQ.at<double>(4, 5) = 99;
+    CQ.at<double>(5, 5) = 99;
+    CQ.at<double>(6, 5) = 99;
+    CQ.at<double>(7, 5) = 99;
+
+    CQ.at<double>(0, 6) = 99;
+    CQ.at<double>(1, 6) = 99;
+    CQ.at<double>(2, 6) = 99;
+    CQ.at<double>(3, 6) = 99;
+    CQ.at<double>(4, 6) = 99;
+    CQ.at<double>(5, 6) = 99;
+    CQ.at<double>(6, 6) = 99;
+    CQ.at<double>(7, 6) = 99;
+
+    CQ.at<double>(0, 7) = 99;
+    CQ.at<double>(1, 7) = 99;
+    CQ.at<double>(2, 7) = 99;
+    CQ.at<double>(3, 7) = 99;
+    CQ.at<double>(4, 7) = 99;
+    CQ.at<double>(5, 7) = 99;
+    CQ.at<double>(6, 7) = 99;
+    CQ.at<double>(7, 7) = 99;
+
+    return CQ;
+}
+
+
+void BAS08::dct1d(const cv::Mat& input, cv::Mat& output){
+
+    assert(( (input.rows == 8) && (input.cols==1) ) && "Column vector of size 8x1 is needed for 1D-DCT.");
+    assert( (input.type() == CV_16S) && "Unable to compute AxDCT-1D: element of type CV_16S required.");
+
+    int16_t x0 = input.at<int16_t>(0,0);
+    int16_t x1 = input.at<int16_t>(1,0);
+    int16_t x2 = input.at<int16_t>(2,0);
+    int16_t x3 = input.at<int16_t>(3,0);
+    int16_t x4 = input.at<int16_t>(4,0);
+    int16_t x5 = input.at<int16_t>(5,0);
+    int16_t x6 = input.at<int16_t>(6,0);
+    int16_t x7 = input.at<int16_t>(7,0);
+
+    output.at<int16_t>(0,0) = x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7;
+    output.at<int16_t>(1,0) = x0 + x1 - x6 - x7;
+    output.at<int16_t>(2,0) = x0 + (x1 >> 1) - (x2 >> 1) - x3 - x4 - (x5 >> 1) + (x6 >> 1) + x7;
+    output.at<int16_t>(3,0) = x5 - x2;
+    output.at<int16_t>(4,0) = x0 - x1 - x2 + x3 + x4 - x5 - x6 + x7;
+    output.at<int16_t>(5,0) = x0 - x1 + x6 - x7;
+    output.at<int16_t>(6,0) = (x0 >> 1) - x1 + x2 - (x3 >> 1) - (x4 >> 1) + x5 - x6 - (x7 >> 1);
+    output.at<int16_t>(7,0) = x4 - x3;
+
+    // std::cerr<<"\nx1 = " <<std::bin <<x1 <<"; x1>>1 = "<<(x1 >> 1);
+}
+
+void BAS08::y_quantizate(const cv::Mat& tile, cv::Mat& output){
+    quantizate(tile, this->getD(), this->getQ(), output);
+}
+
+void BAS08::cr_quantizate(const cv::Mat& tile, cv::Mat& output){
+    quantizate(tile, this->getD(), this->getCQ(), output);
+}
+
+void BAS08::cb_quantizate(const cv::Mat& tile, cv::Mat& output){
+    quantizate(tile, this->getD(), this->getCQ(), output);
+}
+
+void quantizate(const cv::Mat& tile, const cv::Mat& D, const cv::Mat& Q, cv::Mat& output){
+    /*
+        D matrix is merged with the Q matrix. Since D is diagonal, then: 
+        
+            D * (tile) * D' = (diag(D) * diag(D)') .* (tile)
+
+        This result should be divided elem-wise by Q.
+
+        An equivalent quantization is the following:
+        F = tile .* Y
+        where F is the DCT quantizated transformed tile and Y is the following matrix:
+
+        Y = ( diag(D) * diag(D)' ) ./ Q
+
+        Note that the Y matrix can be computed only once, offline.
+        
+    */
+    cv::Mat tileDCT, D_t, DDt;
+    tile.convertTo(tileDCT, CV_64FC1);
+    
+    transpose(D.diag(), D_t);
+    matrix_mult<double>(D.diag(), D_t, DDt, CV_64FC1);
+    // matrix_mult(D_diag, D_t, DDt, CV_64FC1);
+    D_t.deallocate();   //cleanup
+
+    output = tileDCT.mul(DDt);
+    DDt.deallocate(); //cleanup
+    output /= Q;
+
+    /* round */ //TODO: check if this can be done in a non pixel-by-pixel way
+    for(int i=0; i<output.rows; i++){
+        for(int j=0; j<output.cols; j++){
+            output.at<double>(i,j) = round(output.at<double>(i,j));
+        }
+    }
+}
+
+void BAS08::y_dequantizate(const cv::Mat& tile, cv::Mat& output) {
+    dequantizate(tile, this->getQ(), output);
+}
+
+void BAS08::cr_dequantizate(const cv::Mat& tile, cv::Mat& output){
+    dequantizate(tile, this->getCQ(), output);
+}
+
+void BAS08::cb_dequantizate(const cv::Mat& tile, cv::Mat& output){
+    dequantizate(tile, this->getCQ(), output);
+}
+
+void dequantizate(const cv::Mat& tile, const cv::Mat& Q, cv::Mat& output){
+    assert(tile.type() == CV_64FC1 && "Wrong tile type");
+    assert(Q.type() == CV_64FC1 && "Wrong Q type");
+    cv::Mat deq = tile.mul(Q);
+    deq.copyTo(output);
+}
