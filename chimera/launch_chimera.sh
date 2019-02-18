e76db4c6b84264701bde17bf028f1a769201d34b
Andrea Aletto <andrea.aletto8@gmail.com>
2019-02-18 16:09:41 +0100

    check inax1 operator

--- chimera/launch_chimera.sh
+++ chimera/launch_chimera.sh
@@ -2,4 +2,4 @@
 set -x
 
 echo "Launching clang-Chimera..."
-clang-chimera -debug -v -fun-op conf.csv -generate-mutants ../code/src/main.cpp -o output -- -std=c++11 -I../code/include -I/usr/local/include/opencv4 -lopencv_core
+clang-chimera -debug -v -fun-op conf.csv -generate-mutants ../code/src/algorithms/BC12.cpp -o output -- -std=c++11 -I../code/include -I../code/include/algorithms -I../code/include/utils -I../code/include/core -I/usr/local/include/opencv4 -lopencv_core
