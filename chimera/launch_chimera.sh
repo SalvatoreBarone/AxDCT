88a60fd34b299e67dfaa64f1f1fff21712b99258
Andrea Aletto <andrea.aletto8@gmail.com>
2019-02-20 15:14:56 +0100

    fix

--- chimera/launch_chimera.sh
+++ chimera/launch_chimera.sh
@@ -4,6 +4,7 @@ set -x
 echo "Launching clang-Chimera..."
 rm -Rf output
 clang-chimera -debug -v -fun-op conf.csv -generate-mutants  \
+        ../code/src/algorithms/AxDCT_algorithm.cpp          \
         ../code/src/algorithms/BAS08.cpp                    \
         ../code/src/algorithms/BC12.cpp                     \
         -o output --                                        \
