ceb28b2a7eb51592c75381c9424b2e746cc96f87
Andrea Aletto <andrea.aletto8@gmail.com>
2019-03-11 11:56:54 +0100

    works

--- bellerophon/bellerophon_functions.cpp
+++ bellerophon/bellerophon_functions.cpp
@@ -10,45 +10,60 @@
 #include <fstream>
 #include <math.h>
 
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
 
-#include "algorithms_list.h"
-#include "algorithms/BAS08.h"
-#include "core/dct.h"
-#include "core/metrics.h"
+// #include "algorithms_list.h"
+// #include "algorithms/BAS08.h"
+// #include "core/dct.h"
+// #include "core/metrics.h"
+#include <opencv2/opencv.hpp>
 
 
-char oracle_path[]= "./main_oracle.txt";
 
-// extern "C"
-// {
-//     void *__dso_handle = NULL;
-// }
+int nab_68 = 0;
+int nab_67 = 0;
+int nab_66 = 0;
+int nab_65 = 0;
+int nab_64 = 0;
+int nab_63 = 0;
+int nab_62 = 0;
+int nab_61 = 0;
+int nab_60 = 0;
+int nab_59 = 0;
+int nab_58 = 0;
+int nab_57 = 0;
+int nab_56 = 0;
+int nab_55 = 0;
 
-// class aClass{
-//     private:
-//         int * a;
-
-//     public:
-//         aClass() { this->a = new int; }
-//         ~aClass() { delete a; }
-// };
+std::string exec(const char* cmd);
 
 extern "C" double BELLERO_getError()
 {
-    cv::Mat p = cv::Mat::zeros(8,8,0);
+    std::string cmd = "/home/andrea/AxDCT/code/build/bin/psnr -s -i /home/andrea/lena.bmp -x BC12 -n \"nab_55 " + std::to_string(nab_55) + "\"";
+    std::string retstring = exec(cmd.c_str());
+    return stod(retstring);
+    // cv::Mat x = cv::Mat::zeros(8,8, CV_8U);
     return 0.0;
-    // const std::string img_path = "/home/andrea/lena512color.bmp";
-    // const cv::Mat bgrImg = imread( img_path.c_str(), cv::IMREAD_COLOR );
 
-    // cv::Mat dst = bgrImg;
-    // // Direct and inverse transform
-    // transformImage(bgrImg,dst, new BC12 );
-    // inverseTransformImage(dst, dst, new BC12);
- 
- 
-    // // Compute PSNR and return it
-    // return compute_mse(bgrImg, dst);
+}
 
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
 }
 
 
