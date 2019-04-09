3681114af869253f6f78460919b745ea62037768
Andrea Aletto <andrea.aletto8@gmail.com>
2019-04-09 11:09:05 +0200

    fix

--- code/src/zybo_drivers/bc12_zybo.cpp
+++ code/src/zybo_drivers/bc12_zybo.cpp
@@ -29,12 +29,10 @@
 
 //#define __DEBUG__
 
-static void setComponent(uint32_t* bc12_phy_addr, int i, int j, int16_t val);
-static int16_t getComponent(uint32_t* bc12_phy_addr, int i, int j);
-
 void BC12_zybo::dct(const cv::Mat& input, cv::Mat& output){
-
+#ifdef __DEBUG__
     std::cout<<"\nCalling BC12 Zybo Linux driver...";
+#endif
 
 	int fd;
 	unsigned bc12_base_addr = 0x43C00000;
@@ -72,21 +70,13 @@ void BC12_zybo::dct(const cv::Mat& input, cv::Mat& output){
     for(int i=0; i<input.rows; i++){
         for(int j=0; j<input.cols; j++){
             int16_t val = input.at<int16_t>(i,j);
-	        // *((unsigned *)(ptr + (page_offset + 8*i*0x4 + 0x4*j))) = value;		
-    		// setComponent((unsigned*)(ptr+page_offset),i,j,val);
 			*((uint32_t*)(ptr+page_offset + 4*(8*i+j))) = (uint32_t)val;
-	        // printf("Going to write onto %08x the value %08x\n", bc12_base_addr, value);
         }
     }
-
-	
-
-	// TODO: ci vuole un'attesa?
 
 	/* Read results */
 	for(int i=0; i<input.rows; i++){
         for(int j=0; j<input.cols; j++){
-            // int16_t val = getComponent((unsigned*)(ptr+page_offset),i,j);
 			uint32_t* addr = (uint32_t*)(ptr+page_offset + 64*4 + 4*(8*i+j));
 			output.at<int16_t>(i,j) = (int16_t)(*addr);
         }
@@ -94,16 +84,4 @@ void BC12_zybo::dct(const cv::Mat& input, cv::Mat& output){
 
 	munmap(ptr, page_size);
 	close(fd);
-}
-
-void setComponent(uint32_t* bc12_phy_addr, int i, int j, int16_t val){
-	// BC12_AXI_mWriteReg(bc12_phy_addr, 4*(8*i+j), val);
-	*((uint32_t*)(bc12_phy_addr + 4*(8*i+j))) = (uint32_t)val;
-
-}
-
-int16_t getComponent(uint32_t* bc12_phy_addr, int i, int j){
-	// return (int16_t)(BC12_AXI_mReadReg(bc12_phy_addr, 64*4 + 4*(8*i+j)));
-	uint32_t* addr = (uint32_t*)(bc12_phy_addr + 64*4 + 4*(8*i+j));
-	return (int16_t)(*addr);
 }
